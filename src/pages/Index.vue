85e6087a51f6692e173c0f0f9e35c39bc07a0cba
Philo van Kemenade <philovankemenade@gmail.com>
2020-04-13 21:04:55 +0200

    fix side by side home layout

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,23 +3,30 @@
     <!-- Author intro -->
     <Author :show-title="true" />
 
-    <div class="mw9 center ph3-ns">
-      <div class="cf ph2-ns">
-        <div class="fl w-100 w-50-ns pa2">
-          <g-link to="/paintings">
-            <g-image alt="Paintings" class="" src="/images/uploads/200_x_150_cm_Hooded_and_BootedFINAL.jpg" />
-          </g-link>
-          <g-link to="/paintings">Paintings</g-link>
-        </div>
-        <div class="fl w-100 w-50-ns pa2">
-          <g-link to="/drawings">
-            <g-image alt="Drawings" class="" src="/images/uploads/there-ain-t-no-trouble-in-paradise.jpg" />
-          </g-link>
-          <g-link to="/drawings">Drawings</g-link>
-        </div>
+    <div class="flex justify-center">
+      <div class="mh3">
+        <g-link to="/paintings" class="db">
+          <h2>Paintings</h2>
+        </g-link>
+        <g-link to="/paintings">
+          <g-image
+            alt="Paintings"
+            src="/images/uploads/200_x_150_cm_Hooded_and_BootedFINAL.jpg"
+          />
+        </g-link>
+      </div>
+      <div class="mh3">
+        <g-link to="/drawings" class="db">
+          <h2>Drawings</h2>
+        </g-link>
+        <g-link to="/drawings">
+          <g-image
+            alt="Drawings"
+            src="/images/uploads/there-ain-t-no-trouble-in-paradise.jpg"
+          />
+        </g-link>
       </div>
     </div>
-
   </Layout>
 </template>
 
@@ -35,15 +42,22 @@ query {
 </page-query>
 
 <script>
-import Author from '~/components/Author.vue'
-import ArtworkCard from '~/components/ArtworkCard.vue'
+import Author from "~/components/Author.vue";
+import ArtworkCard from "~/components/ArtworkCard.vue";
 
 export default {
   components: {
     Author,
-    ArtworkCard
+    ArtworkCard,
   },
   metaInfo: {
-    title: 'Home'
-  }}
+    title: "Home",
+  },
+};
 </script>
+
+<style scoped>
+img {
+  max-height: 100vh;
+}
+</style>
