be074ea337a691db188673f525f9c07c3e721e00
Philo van Kemenade <philovankemenade@gmail.com>
2020-04-13 21:32:54 +0200

    apply grid layout to Drawings Page

--- src/pages/Drawings.vue
+++ src/pages/Drawings.vue
@@ -4,10 +4,14 @@
     <Author :show-title="true" />
 
     <!-- List drawings -->
-    <div class="drawings">
-      <ArtworkCard v-for="edge in $page.drawings.edges" :key="edge.node.id" :artwork="edge.node"/>
+    <div class="flex relative w-100 flex-wrap justify-center ph2">
+      <ArtworkCard
+        v-for="edge in $page.drawings.edges"
+        :key="edge.node.id"
+        :artwork="edge.node"
+        class="w-100 w-40-m w-20-l ma2"
+      />
     </div>
-
   </Layout>
 </template>
 
@@ -39,16 +43,16 @@
 </page-query>
 
 <script>
-import Author from '~/components/Author.vue'
-import ArtworkCard from '~/components/ArtworkCard.vue'
+import Author from "~/components/Author.vue";
+import ArtworkCard from "~/components/ArtworkCard.vue";
 
 export default {
   components: {
     Author,
-    ArtworkCard
+    ArtworkCard,
   },
   metaInfo: {
-    title: 'Drawings'
-  }
-}
+    title: "Drawings",
+  },
+};
 </script>
