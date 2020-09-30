f19549eff39e6c077afa5e2d068067a7fe706870
Philo van Kemenade <philovankemenade@gmail.com>
2020-09-30 23:10:05 +0200

    fix navbar wrap

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -1,13 +1,11 @@
 <template>
 	<nav class="mv3 navbar">
-		<a href="/paintings">Paintings</a>
-		<a href="/drawings">Drawings</a>
-    <a href="/cv">CV</a>
-    <a href="/about">About</a>
-		<a target="_blank" href="https://instagram.com/irieezamble/"
-			>Instagram&nbsp;➚</a
-		>
-		<a target="_blank" href="mailto:irieezamble@gmail.com">Email&nbsp;➚</a>
+		<a class="dib" href="/paintings">Paintings</a>
+		<a class="dib" href="/drawings">Drawings</a>
+    <a class="dib" href="/cv">CV</a>
+    <a class="dib" href="/about">About</a>
+		<a class="dib" target="_blank" href="https://instagram.com/irieezamble/">Instagram&nbsp;➚</a>
+		<a class="dib" target="_blank" href="mailto:irieezamble@gmail.com">Email&nbsp;➚</a>
 	</nav>
 </template>
 
