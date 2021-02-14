058428d24f5b9e4ceb4c1d8d9819953bc5d488f5
Philo van Kemenade <philovankemenade@gmail.com>
2021-02-14 19:16:52 +0100

    update cv

--- src/pages/CV.vue
+++ src/pages/CV.vue
@@ -9,22 +9,24 @@
         Lives and works in Rotterdam, The Netherlands
       </p>
       
-      <h2>Education</h2>
+      <h2>Education / Residencies</h2>
       <ul class="list ma0">
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2015 - 2019</strong> Bachelor of Fine Arts, Hogeschool voor de Kunsten Utrecht (with honours)</li>
+        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020 - 2021</strong> Roodkapje Rotterdam</li>
+        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Museum het Rembrandthuis, Open studio, together with Timothy Voges</li>
+        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2015 - 2019</strong>  Bachelor of Fine Arts, Hogeschool voor de Kunsten Utrecht (with honours)</li>
       </ul>
 
 
       <h2>Upcoming exhibitions</h2>
       <ul class="list ma0">
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Koninklijke schilderprijs, Amsterdam</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> KunstRAI, Amsterdam</li>
         <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2021</strong> Solo exhibition, Roodkapje, Rotterdam</li>
         <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2021</strong> Solo exhibition, SBK, Amsterdam</li>
+        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2021</strong> KunstRAI, Amsterdam</li>
       </ul>
       
       <h2>Selected exhibitions</h2>
       <ul class="list ma0">
+        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Read the room - Roodkapje Rotterdam, duo show with Kenneth Aidoo</li>
         <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Koninklijke schildersprijs - Koninklijk paleis Amsterdam</li>
         <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> SOFTART - SBK Amsterdam</li>
         <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> With open arms - Roodkapje Rotterdam</li>
@@ -41,28 +43,19 @@
         <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2017</strong> Skandl features #3 - Studio Sarphaat, Amsterdam</li>
       </ul>
       
-      <h2>Awards</h2>
+      <h2>Nominations / Awards</h2>
       <ul class="list ma0">
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2019</strong> SBK Sprouts Young Talent Award</li>
-      </ul>
-      
-      <h2>Nominations</h2>
-      <ul class="list ma0">
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Koninklijke schilderprijs</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Buning Brongers Prijs</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2019</strong> De Scheffer Nominatie</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2019</strong> SBK Sprouts Young Talents Award</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2019</strong> Otto Hetterscheid Stimuleringsprijs</li>
-      </ul>
-      
-      <h2>Residencies</h2>
-      <ul class="list ma0">
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Museum het Rembrandthuis, Open studio, together with Timothy Voges</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020 - 2021</strong> Roodkapje Rotterdam</li>
+        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> <span>Royal Prize of Painting <span class="i">Nominee</span></span></li>
+        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> <span>Buning Brongers Prijs <span class="i">Nominee</span> </span></li>
+        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2019</strong> <span>De Scheffer <span class="i">Nominee</span></span></li>
+        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2019</strong> <span>SBK Sprouts Young Talents Award <span class="i">Awarded</span></span></li>
       </ul>
       
       <h2>Projects</h2>
       <ul class="list ma0">
+        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Curating guest lecturer program HKU</li>
+        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Curating show "Amuse bouche" at the Academie Gallery in Utrecht. An exhibition with Arash Fakhim, Nokukhanya Langa, Narges Mohammadi, Rory Pilgrim and Victoria Sin.</li>
+        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Tirade #481 cover and illustrations https://www.vanoorschot.nl/tirade/nummer-481-tirade-481/</li>
         <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Mural, Kolenkit neighborhood, Amsterdam</li>
         <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Summerschool (painting workshop) - Amsterdam Museum x Studiezalen</li>
         <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2019</strong> Van Gogh museum - Freelance workshop painting and guided tours for students</li>
@@ -72,9 +65,21 @@
 
       <h2>Media</h2>
       <ul class="list ma0">
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> <a href="https://www.vogue.nl/cultuur/cultuur-interviews/a32904717/blacklivesmatter-kunst-zwarte-makers-creatieven/">Vogue - #VogueStories: deze kunstenaars doen je het belang van 'Black Lives Matter' nóg beter begrijpen</a></li>
+        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> <span><a href="https://dezwijger.nl/programma/visibility" target="_blank">Visibility</a> - <b>Pakhuis de Zwijger</b> <br>How do you use your creative work to make the "invisible" visible and also put yourself on the map?</span></li>
+        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2019</strong> <span><a href="https://museumnacht.amsterdam/programma/164/alkebulan-presents-identity-culture- art-talk/" target="_blank">Alkebulan presents: Identity, Culture & Art Talk</a> - <b>Museumnacht Amsterdam, Van Gogh Museum</b>.</span></li>
+        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2019</strong> <span>How your jobs shapes your identity - <b>Soho house Amsterdam</b></span></li>
+        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2019</strong> <span>Museum ft. Instagram - <b>Pakhuis de Zwijger</b> <br>Looking at art in times of selfie culture; looking for the balance between art experience and instagram experiences.</span></li>
+        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2019</strong> <span><a href="https://dezwijger.nl/programma/uncovering-creatives" target="_blank">(Un)covering creatives</a> - <b>Pakhuis de Zwijger</b> <br>New generation of creators of color are conquering the big cities.</span></li>
+        <!-- <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> <a href="https://www.vogue.nl/cultuur/cultuur-interviews/a32904717/blacklivesmatter-kunst-zwarte-makers-creatieven/">Vogue - #VogueStories: deze kunstenaars doen je het belang van 'Black Lives Matter' nóg beter begrijpen</a></li>
         <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2018</strong> <a href="https://www.nrc.nl/nieuws/2018/11/29/de-galerie-op-je-scherm-2-a2828826">NRC Handelsblad - Hoe Instagram de kunstmarkt opschudt</a></li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2018</strong> <a href="https://schonmagazine.com/the-panther/">Schön Magazine! - the panther</a></li>
+        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2018</strong> <a href="https://schonmagazine.com/the-panther/">Schön Magazine! - the panther</a></li> -->
+      </ul>
+      
+      <h2>Collections</h2>
+      <ul class="list ma0">
+        <li>Ministry of Foreign affairs </li>
+        <li>KPMG</li>
+        <li>Isala Hospital Zwolle</li>
       </ul>
       
 
