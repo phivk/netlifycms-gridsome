dc172b8a3be87977f7873fca2e6eede8b19b5523
Philo van Kemenade <philovankemenade@gmail.com>
2021-04-19 22:19:09 +0200

    prettier format CV & update Media section

--- src/pages/CV.vue
+++ src/pages/CV.vue
@@ -5,79 +5,253 @@
     <div class="measure-wide center pa2">
       <h1>CV</h1>
       <p class="">
-        Born and raised in Amsterdam <br>
+        Born and raised in Amsterdam <br />
         Lives and works in Rotterdam, The Netherlands
       </p>
-      
+
       <h2>Education</h2>
       <ul class="list ma0">
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2015 - 2019</strong>  Bachelor of Fine Arts, Hogeschool voor de Kunsten Utrecht (with honours)</li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2015 - 2019</strong>
+          Bachelor of Fine Arts, Hogeschool voor de Kunsten Utrecht (with
+          honours)
+        </li>
       </ul>
 
       <h2>Residencies</h2>
       <ul class="list ma0">
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020 - 2021</strong> Roodkapje Rotterdam</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Museum het Rembrandthuis, Open studio, together with Timothy Voges</li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2020 - 2021</strong>
+          Roodkapje Rotterdam
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Museum het
+          Rembrandthuis, Open studio, together with Timothy Voges
+        </li>
       </ul>
 
       <h2>Upcoming exhibitions</h2>
       <ul class="list ma0">
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2021</strong> Solo exhibition, Roodkapje, Rotterdam</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2021</strong> Solo exhibition, SBK, Amsterdam</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2021</strong> KunstRAI, Amsterdam</li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2021</strong> Solo
+          exhibition, Roodkapje, Rotterdam
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2021</strong> Solo
+          exhibition, SBK, Amsterdam
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2021</strong> KunstRAI,
+          Amsterdam
+        </li>
       </ul>
-      
+
       <h2>Selected exhibitions</h2>
       <ul class="list ma0">
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Read the room - Roodkapje Rotterdam, duo show with Kenneth Aidoo</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Koninklijke schildersprijs - Koninklijk paleis Amsterdam</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> SOFTART - SBK Amsterdam</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> With open arms - Roodkapje Rotterdam</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> HIER. Zwart in Rembrandts tijd - Rembrandthuis, Amsterdam</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Shoulda, Coulda, Woulda - Roodkapje, Rotterdam</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2019</strong> Sidewalk Talk - EXboot, Utrecht (solo)</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2019</strong> Vers bij MIJ - Museum IJsselstein, IJsselstein</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2019</strong> Sprouts Young Talent - SBK, Amsterdam</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2019</strong> Nijmeegse Kunstnacht x Afrika Museum - Expoplu, Nijmegen</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2019</strong> Art Utrecht - Pandhof Domkerk, Utrecht</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2019</strong> Lichting 2019 - Onze keuze, Galerie Wit, Wageningen</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2019</strong> HKU Exposure - Utrecht</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2018</strong> Feminist Art Fest - LAB111, Amsterdam</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2017</strong> Skandl features #3 - Studio Sarphaat, Amsterdam</li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Read the room
+          - Roodkapje Rotterdam, duo show with Kenneth Aidoo
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Koninklijke
+          schildersprijs - Koninklijk paleis Amsterdam
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2020</strong> SOFTART - SBK
+          Amsterdam
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2020</strong> With open
+          arms - Roodkapje Rotterdam
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2020</strong> HIER. Zwart
+          in Rembrandts tijd - Rembrandthuis, Amsterdam
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Shoulda,
+          Coulda, Woulda - Roodkapje, Rotterdam
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2019</strong> Sidewalk Talk
+          - EXboot, Utrecht (solo)
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2019</strong> Vers bij MIJ
+          - Museum IJsselstein, IJsselstein
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2019</strong> Sprouts Young
+          Talent - SBK, Amsterdam
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2019</strong> Nijmeegse
+          Kunstnacht x Afrika Museum - Expoplu, Nijmegen
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2019</strong> Art Utrecht -
+          Pandhof Domkerk, Utrecht
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2019</strong> Lichting 2019
+          - Onze keuze, Galerie Wit, Wageningen
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2019</strong> HKU Exposure
+          - Utrecht
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2018</strong> Feminist Art
+          Fest - LAB111, Amsterdam
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2017</strong> Skandl
+          features #3 - Studio Sarphaat, Amsterdam
+        </li>
       </ul>
-      
+
       <h2>Nominations / Awards</h2>
       <ul class="list ma0">
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> <span>Royal Prize of Painting <span class="i">Nominee</span></span></li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> <span>Buning Brongers Prijs <span class="i">Nominee</span> </span></li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2019</strong> <span>De Scheffer <span class="i">Nominee</span></span></li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2019</strong> <span>SBK Sprouts Young Talents Award <span class="i">Awarded</span></span></li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2020</strong>
+          <span>Royal Prize of Painting <span class="i">Nominee</span></span>
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2020</strong>
+          <span>Buning Brongers Prijs <span class="i">Nominee</span> </span>
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2019</strong>
+          <span>De Scheffer <span class="i">Nominee</span></span>
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2019</strong>
+          <span
+            >SBK Sprouts Young Talents Award
+            <span class="i">Awarded</span></span
+          >
+        </li>
       </ul>
-      
+
       <h2>Projects</h2>
       <ul class="list ma0">
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Curating guest lecturer program HKU</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Curating show "Amuse bouche" at the Academie Gallery in Utrecht. An exhibition with Arash Fakhim, Nokukhanya Langa, Narges Mohammadi, Rory Pilgrim and Victoria Sin.</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Tirade #481 cover and illustrations https://www.vanoorschot.nl/tirade/nummer-481-tirade-481/</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Mural, Kolenkit neighborhood, Amsterdam</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Summerschool (painting workshop) - Amsterdam Museum x Studiezalen</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2019</strong> Van Gogh museum - Freelance workshop painting and guided tours for students</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2018</strong> Production and curator assistent: 'Who are we again?' Arti et amicitiae. An exhibition with artists: Amie Dicke, Berend Strik, Charl Landvreugd, Charlott Markus,Evelyn Taocheng Wang, Femmy Otten, Lucas Lenglet, Marlene Dumas, René Bosch,Sander Breure &amp; Witte van Hulzen, Sharieta Berghuis en Tyna Adebowale.</li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2018</strong> Guest teacher IMC de weekend school, Amsterdam Zuid-Oost &amp; de Weekendschool, Utrecht</li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Curating
+          guest lecturer program HKU
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Curating show
+          "Amuse bouche" at the Academie Gallery in Utrecht. An exhibition with
+          Arash Fakhim, Nokukhanya Langa, Narges Mohammadi, Rory Pilgrim and
+          Victoria Sin.
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Tirade #481
+          cover and illustrations
+          https://www.vanoorschot.nl/tirade/nummer-481-tirade-481/
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Mural,
+          Kolenkit neighborhood, Amsterdam
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2020</strong> Summerschool
+          (painting workshop) - Amsterdam Museum x Studiezalen
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2019</strong> Van Gogh
+          museum - Freelance workshop painting and guided tours for students
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2018</strong> Production
+          and curator assistent: 'Who are we again?' Arti et amicitiae. An
+          exhibition with artists: Amie Dicke, Berend Strik, Charl Landvreugd,
+          Charlott Markus,Evelyn Taocheng Wang, Femmy Otten, Lucas Lenglet,
+          Marlene Dumas, René Bosch,Sander Breure &amp; Witte van Hulzen,
+          Sharieta Berghuis en Tyna Adebowale.
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2018</strong> Guest teacher
+          IMC de weekend school, Amsterdam Zuid-Oost &amp; de Weekendschool,
+          Utrecht
+        </li>
       </ul>
 
       <h2>Media</h2>
       <ul class="list ma0">
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> <span><a href="https://dezwijger.nl/programma/visibility" target="_blank">Visibility</a> - <b>Pakhuis de Zwijger</b> <br>How do you use your creative work to make the "invisible" visible and also put yourself on the map?</span></li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2019</strong> <span><a href="https://museumnacht.amsterdam/programma/164/alkebulan-presents-identity-culture- art-talk/" target="_blank">Alkebulan presents: Identity, Culture & Art Talk</a> - <b>Museumnacht Amsterdam, Van Gogh Museum</b>.</span></li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2019</strong> <span>How your jobs shapes your identity - <b>Soho house Amsterdam</b></span></li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2019</strong> <span>Museum ft. Instagram - <b>Pakhuis de Zwijger</b> <br>Looking at art in times of selfie culture; looking for the balance between art experience and instagram experiences.</span></li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2019</strong> <span><a href="https://dezwijger.nl/programma/uncovering-creatives" target="_blank">(Un)covering creatives</a> - <b>Pakhuis de Zwijger</b> <br>New generation of creators of color are conquering the big cities.</span></li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2020</strong> <a href="https://www.vogue.nl/cultuur/cultuur-interviews/a32904717/blacklivesmatter-kunst-zwarte-makers-creatieven/">Vogue - #VogueStories: deze kunstenaars doen je het belang van 'Black Lives Matter' nóg beter begrijpen</a></li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2018</strong> <a href="https://www.nrc.nl/nieuws/2018/11/29/de-galerie-op-je-scherm-2-a2828826">NRC Handelsblad - Hoe Instagram de kunstmarkt opschudt</a></li>
-        <li class="flex-ns"><strong class="dib-ns w4-ns flex-shrink-0">2018</strong> <a href="https://schonmagazine.com/the-panther/">Schön Magazine! - the panther</a></li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2021</strong>
+          <span
+            >De Volkskrant -
+            <a
+              href="https://www.volkskrant.nl/cultuur-media/vrouwen-zijn-in-de-kunst-nog-altijd-zwaar-ondervertegenwoordigd-dit-zijn-er-een-paar-die-u-moet-kennen~bfdc409c/"
+              target="_blank"
+              >Vrouwen zijn in de kunst nog altijd zwaar ondervertegenwoordigd.
+              Dit zijn er een paar die u moet kennen</a
+            ></span
+          >
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2020</strong>
+          <a
+            href="http://www.metropolism.com/nl/features/42232_play_and_fight_iri_e_zambl_and_the_importance_of_taking_up_your_space"
+            >Metropolis M - Play and Fight: Iriée Zamblé and the importance of taking up your space</a
+          >
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2020</strong>
+          <a
+            href="https://www.parool.nl/ps/de-muren-van-west-ik-hoop-dat-de-buurt-zich-erin-herkent~b91df7939/"
+            >Het Parool - De Muren van West: ‘Ik hoop dat de buurt zich erin herkent’ </a
+          >
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2020</strong>
+          <a
+            href="https://www.harpersbazaar.com/nl/cultuur-reizen/a34561595/rembrandthuis-jonge-schilders/"
+            >Harper's Bazaar - Na 400 jaar is het Rembrandthuis opnieuw een plek
+            voor jonge kunstenaars</a
+          >
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2020</strong>
+          <a
+            href="https://www.trouw.nl/binnenland/hoe-schildert-jong-de-ouderdom-met-eigenzinnige-hoofddeksels-en-opvallende-kleuren~b4ec54e4/"
+            >Trouw - Hoe schildert jong de ouderdom? Met eigenzinnige
+            hoofddeksels en opvallende kleuren</a
+          >
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2020</strong>
+          <a
+            href="https://www.vogue.nl/cultuur/cultuur-interviews/a32904717/blacklivesmatter-kunst-zwarte-makers-creatieven/"
+            >Vogue - #VogueStories: deze kunstenaars doen je het belang van
+            'Black Lives Matter' nóg beter begrijpen</a
+          >
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2020</strong>
+          <a
+            href="https://www.nrc.nl/nieuws/2018/11/29/de-galerie-op-je-scherm-2-a2828826"
+            >Het Parool - Organisatie over grote opkomst protest: ‘Nog steeds
+            overwhelmed’
+          </a>
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2018</strong>
+          <a
+            href="https://www.nrc.nl/nieuws/2018/11/29/de-galerie-op-je-scherm-2-a2828826"
+            >NRC Handelsblad - Hoe Instagram de kunstmarkt opschudt</a
+          >
+        </li>
+        <li class="flex-ns">
+          <strong class="dib-ns w4-ns flex-shrink-0">2018</strong>
+          <a href="https://schonmagazine.com/the-panther/"
+            >Schön Magazine! - the panther</a
+          >
+        </li>
       </ul>
-      
+
       <h2>Collections</h2>
       <ul class="list ma0">
         <li>Museum het Rembrandthuis</li>
@@ -86,7 +260,6 @@
         <li>Isala Hospital Zwolle</li>
       </ul>
     </div>
-
   </Layout>
 </template>
 <script>
