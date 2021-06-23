3479863ab494961a3840fbac69e98306b69e5c03
Philo van Kemenade <philovankemenade@gmail.com>
2021-06-23 18:46:12 +0200

    update about

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -6,11 +6,11 @@
       <h1>About</h1>
       <h2>ENG</h2>
       <p>
-        Iriée Zamblé (1995) takes notice of the lack of dignified images of Afro-Dutch within a Dutch context, Zamblé’s paintings are informed by the people she knows and passes by everyday, and the lack of interest that seems to surround them. By appropriating details of their appearance, Zamblé forms new and powerful figures. She praises control and ownership through her paintings and influences the collective way black consciousness is shaped. 
+        Iriée Zamblé (1995) forms dignified images of Afro-Dutch people. Zamblé’s paintings and drawings are informed by the people she knows and passes by everyday, and the lack of interest that seems to surround them. By appropriating details of their appearance, Zamblé forms new and powerful figures. She praises control and ownership through her paintings and influences the collective way black consciousness is shaped.
       </p>
       <h2>NL</h2>
       <p> 
-        Iriée Zamblé (1995) signaleert het gebrek aan waardige beelden van Afro-Nederlanders binnen een Nederlandse context. Haar schilderijen en tekeningen zijn geïnspireerd door de mensen die zij dagelijks passeert, en het gebrek aan interesse dat hen lijkt te omringen. Door details van hun uiterlijk toe te eigenen, vormt Zamblé nieuwe en krachtige figuren. Ze staat voor zelfbeschikking en controle door middel van haar schilderijen en beïnvloedt de collectieve manier waarop het zwarte bewustzijn wordt gevormd. 
+        Iriée Zamblé (1995) maakt waardige beelden van Afro-Nederlanders. Zamblé’s schilderijen en tekeningen zijn geïnspireerd door de mensen die zij dagelijks passeert, en het gebrek aan interesse dat hen lijkt te omringen. Door details van hun uiterlijk toe te eigenen, vormt Zamblé nieuwe en krachtige figuren. Ze staat voor zelfbeschikking en controle door middel van haar schilderijen en beïnvloedt de collectieve manier waarop zwart bewustzijn wordt gevormd.
       </p>
 
       <figure class="mw5">
