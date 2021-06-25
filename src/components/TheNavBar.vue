74535b4eb2f748eefb5c36aeed4ad3affa72ed0f
Philo van Kemenade <philovankemenade@gmail.com>
2021-06-25 10:56:21 +0200

    add shop to nav

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -2,6 +2,7 @@
 	<nav class="mv3 navbar">
 		<a class="dib" href="/paintings">Paintings</a>
 		<a class="dib" href="/drawings">Drawings</a>
+		<a class="dib" href="/shop">Shop</a>
     <a class="dib" href="/cv">CV</a>
     <a class="dib" href="/about">About</a>
 		<a class="dib" target="_blank" href="https://instagram.com/irieezamble/">Instagram&nbsp;➚</a>
