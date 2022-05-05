d5991917a42174463504e773bf41f0b3789355c9
Philo van Kemenade <philovankemenade@gmail.com>
2022-05-05 21:01:46 +0200

    add margin top

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app" class="pa2 pa3-ns">
-
     <header>
       <div>
         <Logo v-if="showLogo" />
@@ -8,28 +7,26 @@
     </header>
 
     <main>
-      <slot/>
+      <slot />
     </main>
 
-    <footer class="footer db tc mv3">
+    <footer class="footer db tc mb3 mt5">
       <span>Copyright © {{ new Date().getFullYear() }} Iriée Zamblé. </span>
     </footer>
-
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Logo from "~/components/Logo.vue";
 
 export default {
   props: {
-    showLogo: { default: true }
+    showLogo: { default: true },
   },
   components: {
-    Logo
-  }
-}
+    Logo,
+  },
+};
 </script>
 
-<style lang="scss">
-</style>
+<style lang="scss"></style>
